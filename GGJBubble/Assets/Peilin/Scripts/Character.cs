67d288f3b8cc2a11562e8760a3cd474f742fceac
peilin20 <peilinhe20@gmail.com>
2025-01-25 11:29:51 -0800

    Added damage system

--- GGJBubble/Assets/Peilin/Scripts/Character.cs
+++ GGJBubble/Assets/Peilin/Scripts/Character.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
     public int hp = 100;
-
+    public TextMeshProUGUI CharacterHP;
+    public Slider HPSlider;
     // Method to decrease HP
     public void DecreaseHP(int damage)
     {
@@ -22,11 +25,33 @@ public class Character : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        UpdateHPUI();
+    }
     // Method called when the character's HP reaches 0
     private void Die()
     {
         Debug.Log("Character has died!");
         // Add your logic for when the character dies (e.g., destroy object, restart level, etc.)
         Destroy(gameObject);
+    }
+    private void UpdateHPUI()
+    {
+        // Update Text
+        if (CharacterHP != null)
+            CharacterHP.text = "HP: " + hp;
+        else
+        {
+            Debug.LogWarning("HP Text is not assigned!");
+        }
+
+        // Update Slider
+        if (HPSlider != null)
+            HPSlider.value = hp;
+        else
+        {
+            Debug.LogWarning("HP slider is not assigned!");
+        }
     }
 }
