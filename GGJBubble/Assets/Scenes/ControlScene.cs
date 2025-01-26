e2d495affa22aeaa221744154ad1d435ba9276fa
yixiaxie <yixiaxie@usc.edu>
2025-01-26 15:05:22 -0800

    opening scene

---
+++ GGJBubble/Assets/Scenes/ControlScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlScene : MonoBehaviour
+{
+    public string nextSceneName = "PolishedVersion"; // 下一个场景的名称
+
+    void Update()
+    {
+        // 检测玩家按下任意键
+        if (Input.anyKeyDown)
+        {
+            // 切换到下一个场景
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
---
+++ GGJBubble/Assets/Scenes/OpeningScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningScene : MonoBehaviour
+{
+    public string nextSceneName = "Control"; // 下一个场景的名称
+
+    void Update()
+    {
+        // 检测玩家按下任意键
+        if (Input.anyKeyDown)
+        {
+            // 切换到下一个场景
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
